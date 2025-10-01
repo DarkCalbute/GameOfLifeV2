42ac84e4ee94e79d131eaf90c1a0f3c1b020d1d7
Nathan FROMAGE <NFR24802@open-groupe.com>
2025-10-01 13:58:46 +0200

    fix movement && fix adding or removing cells

--- src/components/canva/board/Board.tsx
+++ src/components/canva/board/Board.tsx
@@ -2,12 +2,13 @@ import { useEffect, useRef, useState } from "react";
 import { renderCanvas } from "../../../services/renderCanvas";
 import { useGameContext } from "../../../contexts/GameContexts";
 import "./Board.css";
+import { computeNextGeneration } from "../../../services/gameLogic";
 
 const Board: React.FC = () => {
   const {
     cells,
     setCells,
-    paused,
+    isPaused,
     nextTick,
     zoom,
     offsetX,
@@ -23,11 +24,11 @@ const Board: React.FC = () => {
     const canvas = canvasRef.current;
 
     let isMouseDown = false;
+    let isMouseMoving = false;
     let startMouseX = 0;
     let startMouseY = 0;
     let startOffsetX = 0;
     let startOffsetY = 0;
-    const dragThreshold = 5;
 
     if (!canvas) {
       return;
@@ -40,38 +41,11 @@ const Board: React.FC = () => {
 
     const onMouseDown = (e: MouseEvent) => {
       isMouseDown = true;
+      isMouseMoving = false;
       startMouseX = e.clientX;
       startMouseY = e.clientY;
       startOffsetX = offsetX.current;
       startOffsetY = offsetY.current;
-    };
-    
-    const onMouseUp = (e: MouseEvent) => {
-      isMouseDown = false;
-      const dx = e.clientX - startMouseX;
-      const dy = e.clientY - startMouseY;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < dragThreshold) {
-        let newCells = new Set(cells);
-        // click
-        const cellSize = 50;
-        const cursorCellX = Math.floor(
-          (e.clientX - offsetX.current) / (cellSize * zoom.current)
-        );
-        const cursorCellY = -Math.floor(
-          (e.clientY - offsetY.current) / (cellSize * zoom.current)
-        );
-        const cell = `${cursorCellX},${cursorCellY}`;
-
-        if (cells.has(cell)) {
-          newCells.delete(cell);
-        } else {
-          newCells.add(cell);
-        }
-        setCells(newCells);
-        console.log(newCells);
-      }
     };
 
     const onMouseMove = (e: MouseEvent) => {
@@ -80,8 +54,38 @@ const Board: React.FC = () => {
         const dx = e.clientX - startMouseX;
         const dy = e.clientY - startMouseY;
 
+        if (Math.abs(dx) > 2 || Math.abs(dy) > 2) {
+          isMouseMoving = true;
+        }
         offsetX.current = startOffsetX + dx;
         offsetY.current = startOffsetY + dy;
+      }
+    };
+    
+    const onMouseUp = (e: MouseEvent) => {
+      isMouseDown = false;
+
+      // click
+      if (!isMouseMoving) {
+        let newCells = new Set(cells);
+        const cellSize = 50;
+        const cursorCellX = Math.floor(
+          (e.clientX - offsetX.current) / (cellSize * zoom.current)
+        );
+        const cursorCellY = -Math.floor(
+          (e.clientY - offsetY.current) / (cellSize * zoom.current)
+        );
+        const cell = `${cursorCellX},${cursorCellY}`;
+        
+        setCells(prev => {
+          const newCells = new Set(prev);
+          if (newCells.has(cell)) {
+            newCells.delete(cell);
+          } else {
+            newCells.add(cell);
+          }
+          return newCells;
+        });
       }
     };
 
@@ -113,7 +117,7 @@ const Board: React.FC = () => {
       canvas.removeEventListener("wheel", onWheel);
       window.removeEventListener("resize", handleResize);
     }
-  }, [cells, setCells, offsetX, offsetY, zoom]);
+  }, [setCells, offsetX, offsetY, zoom]);
 
   useEffect(() => {
     let lastDraw = 0;
@@ -125,13 +129,14 @@ const Board: React.FC = () => {
     const loop = (timestamp: number) => {
       rafId = requestAnimationFrame(loop);
 
-      if (!paused && timestamp - lastCycle > cycleDuration) {
+      if (!isPaused && timestamp - lastCycle > cycleDuration) {
         nextTick();
         lastCycle = timestamp;
       }
 
       if (timestamp - lastDraw > 1000 / frameRate) {
         const ctx = canvasRef.current?.getContext("2d");
+
         if (ctx) {
           renderCanvas(
             ctx,
@@ -149,7 +154,7 @@ const Board: React.FC = () => {
 
     rafId = requestAnimationFrame(loop);
     return () => cancelAnimationFrame(rafId);
-  }, [paused, cells, nextTick, showGrid, mousePos, zoom, offsetX, offsetY]);
+  }, [cells, nextTick, showGrid, mousePos, zoom, offsetX, offsetY]);
 
   return <canvas ref={canvasRef} className="canvas" />;
 };
--- src/contexts/GameContexts.tsx
+++ src/contexts/GameContexts.tsx
@@ -4,7 +4,7 @@ import { computeNextGeneration } from "../services/gameLogic";
 type GameContextType = {
   cells: Set<string>;
   generation: number;
-  paused: boolean;
+  isPaused: boolean;
   showGrid: boolean;
   zoom: React.MutableRefObject<number>;
   offsetX: React.MutableRefObject<number>;
@@ -23,22 +23,21 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
   const [cells, setCells] = useState<Set<string>>(new Set(["0,-1", "-1,0", "0,0", "0,1", "1,1"]));
   const [cellsHistory, setCellsHistory] = useState<Set<string>[]>([]);
   const [generation, setGeneration] = useState(0);
-  const [paused, setPaused] = useState(true);
+  const [isPaused, setPaused] = useState(true);
   const [showGrid, setShowGrid] = useState(true);
 
   const zoom = useRef(1);
   const offsetX = useRef(window.innerWidth / 2);
   const offsetY = useRef(window.innerHeight / 2);
 
-  const togglePause = () => setPaused(p => !p);
+  const togglePause = () => setPaused(!isPaused);
 
   const nextTick = () => {
-    setCells(prev => {
-      const next = computeNextGeneration(prev);
-      setCellsHistory(h => [...h, prev]);
-      setGeneration(g => g + 1);
-      return next;
-    });
+    const newCells = computeNextGeneration(cells);
+
+    //setCellsHistory(h => [...h, cells]);
+    setGeneration(g => g + 1);
+    setCells(newCells);
   };
 
   const prevTick = () => {
@@ -61,13 +60,20 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const toggleGrid = () => setShowGrid(g => !g);
+  
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+      e.preventDefault();
+      togglePause()
+    }
+  });
 
   return (
     <GameContext.Provider
       value={{
         cells,
         generation,
-        paused,
+        isPaused,
         showGrid,
         zoom,
         offsetX,
